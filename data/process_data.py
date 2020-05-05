7d5e3994bbc78194c0a1b286f3c819fa7b16b9a6
ecoudron <55484185+ecoudron@users.noreply.github.com>
2020-05-06 00:12:27 +0200

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
-
+# import libraries
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
 
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # Merge datasets
+    df = pd.merge(messages, categories, on="id")
+    return df
 
 def clean_data(df):
-    pass
+    # Drop drop_duplicates
+    df.drop_duplicates(inplace=True)
+
+    # Split categories column
+    categories = df.categories.str.split(";",expand=True)
+
+    # Extract columns names from the first row
+    categories.columns = categories.iloc[0,].str.split("-",expand=True)[0]
+
+    # Keep only 0/1 from each value
+    for column in categories:
+        categories[column] = categories[column].str[-1].astype(int)
+
+    # Remove the previous categories column and replace with the new ones
+    df.drop(columns=["categories"], inplace=True)
+    df = pd.concat([df,categories], axis=1)
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(database_filename)
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():
@@ -24,12 +49,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
